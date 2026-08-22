c3127d9501bc88f9f2dfe62ece9b4c4368ccd342
G-Narendra <narendra@dev-agent.local>
2026-08-23 05:12:53 +0530

    Create portfolio/server.js

--- portfolio/server.js
+++ portfolio/server.js
@@ -1,25 +1,25 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.get('/', (req, res) => {
-  res.render('index');
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
-app.get('/about', (req, res) => {
-  res.render('about');
+app.get("/about", (req, res) => {
+  res.render("about");
 });
 
-app.get('/gallery', (req, res) => {
-  res.render('gallery');
+app.get("/gallery", (req, res) => {
+  res.render("gallery");
 });
 
-app.get('/contact', (req, res) => {
-  res.render('contact');
+app.get("/contact", (req, res) => {
+  res.render("contact");
 });
 
 app.listen(port, () => {
-  console.log(`Server started on port ${port}`);
+  console.log(`Server started on port ${port}");
 });
